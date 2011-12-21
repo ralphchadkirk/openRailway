4b09e4ad04ff8edfa44074136fe2e1ca50d57787
Ralph Chadkirk <ralphchadkirk@gmail.com>
2011-12-21 22:02:47 +0000

    theme
    started css and header files

---
+++ theme/or-header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF8">
+        <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link href="<?php //Root to CSS here ?>style.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <header>
+            <div id="logo">
+                <h1 id="head">openRailway</h1>
+            </div>
+            <div id="login">
+                <div id="login-inner">
+                    You are not logged in
+                </div>
+            </div>
+            <div class="clear"></div>
+        </header>
