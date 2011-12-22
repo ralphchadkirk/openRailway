8b38fc1ad36b1fa2da92726da3ce08e67bfc8fee
Ralph Chadkirk <ralphchadkirk@gmail.com>
2011-12-22 09:38:42 +0000

    header work
    adding text and title

--- theme/or-header.php
+++ theme/or-header.php
@@ -6,11 +6,12 @@
             <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
         <link href="<?php //Root to CSS here ?>style.css" type="text/css" rel="stylesheet" />
+        <title><?php //get_page_title() ?></title>
     </head>
     <body>
         <header>
             <div id="logo">
-                <h1 id="head">openRailway</h1>
+                <h1 id="head">openRailway <span id="head-thin">Open source tools for heritage railways</span></h1>
             </div>
             <div id="login">
                 <div id="login-inner">
