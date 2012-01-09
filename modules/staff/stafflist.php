7c92519ab102ef1b1eb77fd6ff5c55f71b6829d3
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-01-09 21:09:56 +0000

    Staff management code change

--- modules/staff/stafflist.php
+++ modules/staff/stafflist.php
@@ -3,5 +3,13 @@
 	openRailway_init();
 	getModuleConfig("staff");
 	page_header("Staff List",$module['name'],$module['directory'],$module['css']);
+	
+	$template = new Template();
+    $template->set_custom_template('html','default');
+	$template->set_filenames(array(
+                                   'stafflist' => 'stafflist.html'
+                                   ));
+    $template->display('stafflist');
+    
 	page_footer();
 ?>
