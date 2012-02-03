ff259e6a6c99d5ba2fe6b0cc1a9feb5474cce3ca
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-03 16:19:53 +0000

    Breadcrumbs code (DNW)

--- lib/functions.php
+++ lib/functions.php
@@ -10,21 +10,19 @@
 	{
 		global $name;
 		$name = array();
-		if(!isset($id))
-		{
-			$id = $row['folder_id'];
-		}
 		$sql = "SELECT * FROM `doclib_folders` WHERE `folder_id` = '" . $id . "'";
 		$result = mysql_query($sql);
 		$row = mysql_fetch_assoc($result);
 		
-		if($row['parent_id'] == 0)
+		if($row['parent_id'] == "")
 		{
 			return null;
 			die();
 		} else
 		{
 			array_push($name,$row['folder_name']);
+			doclibBreadcrumbArray($row['folder_id']);
+			die();
 		}
 	}
 ?>
