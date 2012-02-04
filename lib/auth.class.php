598b28989150548bf76deddc091527651517f4c1
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-04 20:01:29 +0000

    Log user in functions

--- lib/auth.class.php
+++ lib/auth.class.php
@@ -1,6 +1,17 @@
 <?php
 	class Authentication extends openRailwayCore
 	{
+		private function genRandomString()
+		{
+    		$length = 10;
+    		$characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+    		$string = ”;    
+    		for ($p = 0; $p < $length; $p++)
+    		{
+       			$string .= $characters[mt_rand(0, strlen($characters))];
+    		}
+   			return $string;
+		}
 		public static function blockPageToVisitors()
 		{
 			if(!isset($_SESSION['session_id']))
@@ -16,8 +27,25 @@
                 die();
 			}
 		}
-		public static function logUserIn()
+		public static function logUserIn($username,$password)
 		{
+			openRailwayCore::dbConnect();
+			$query = "SELECT `user_id` `username` `password` FROM `users` WHERE `username` = '" . $username . "' AND `password` = MD5(" . $password . ")";
+			$result = mysql_query($query);
+			$row = mysql_fetch_assoc($result);
+			if($username == $row['username'] && $password == $row['password'])
+			{
+				$_SESSION['session_id'] = genRandomString();
+				$_SESSION['user_id'] = $row['user_id'];
+				$_SESSION['log_in_time'] = time();
+				$query = "INSERT INTO sessions(session_id,log_in_time,user_id) VALUES (" . $_SESSION['session_id'] . "," . $_SESSION['log_in_time'] . "," . $_SESSION['user_id'] . ")";
+				$result = mysql_query($query);
+				header("Location: " . ROOT . "index.php");
+			}
+			else 
+			{
+				header("Location: " . ROOT . "index.php?l=fail");
+			}
 		}
 		public static function logUserOut()
 		{
