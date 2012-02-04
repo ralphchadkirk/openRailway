a4432b4c8caaec34a3264c3c12aba3b5a4349afb
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-04 19:20:45 +0000

    Authentication class

--- lib/auth.class.php
+++ lib/auth.class.php
@@ -1,6 +1,26 @@
 <?php
-	class authentication extends openRailwayCore
+	class Authentication extends openRailwayCore
 	{
-		
+		public static function blockPageToVisitors()
+		{
+			if(!isset($_SESSION['session_id']))
+			{
+				openRailwayCore::pageHeader("Access not authorised");
+				$template = new Template;
+				$template->set_custom_template('theme','default');
+				$template->set_filenames(array(
+                                   				'body' => 'login.html'
+                                   			));
+                $template->display('body');
+                openRailwayCore::pageFooter();
+                die();
+			}
+		}
+		public static function logUserIn()
+		{
+		}
+		public static function logUserOut()
+		{
+		}
 	}
 ?>
