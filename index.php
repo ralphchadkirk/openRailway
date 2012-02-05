f544d59672a74b62970bb0ba3b08ad31c26a0730
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-05 08:13:10 +0000

    If logout var exists, log user out.

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     {
     	Authentication::logUserIn($_POST['username'],$_POST['password']);
     }
+    if(isset($_GET['logout']))
+    {
+    	Authentication::logUserOut();
+    }
     Authentication::blockPageToVisitors();
     
     // Check to see if a system message is set
