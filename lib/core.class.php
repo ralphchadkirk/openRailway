60c22fda3af4f1e56d2544df6f4fbd170f5a7fb6
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-06 19:22:47 +0000

    Sort out tabs

--- lib/core.class.php
+++ lib/core.class.php
@@ -45,10 +45,10 @@
                   		
 			if(isset($modulename))
 			{
-            	$t = $title . " - " . $modulename;
+				$t = $title . " - " . $modulename;
 			} else
 			{
-                $t = $title;
+				$t = $title;
 			}
                   		
 			$template = new Template();
@@ -58,12 +58,12 @@
 			$template->assign_var('ROOT',ROOT);
 			if(isset($_SESSION['session_id']))
 			{
-            	$query = "SELECT * FROM `staff_master` WHERE `staff_id` = '" . $_SESSION['staff_id'] . "'";
-            	$result = mysql_query($query);
-            	$row = mysql_fetch_assoc($result);
-            	$template->assign_block_vars('if_user_logged_in',array(
-            															'NAME' => $row['first_name'] . " " . $row['surname'],
-            														));
+				$query = "SELECT * FROM `staff_master` WHERE `staff_id` = '" . $_SESSION['staff_id'] . "'";
+				$result = mysql_query($query);
+				$row = mysql_fetch_assoc($result);
+				$template->assign_block_vars('if_user_logged_in',array(
+																		'NAME' => $row['first_name'] . " " . $row['surname'],
+																		));
             }
                         
 			// Display list of modules in /modules
@@ -79,38 +79,38 @@
 			}
 			foreach($names as $name)
 			{
-                // Get the module config details
+				// Get the module config details
 				$path = FROOT . "modules/" . $name . "/";	
 				$module = parse_ini_file($path . "module.cfg");
-                $template->assign_block_vars("module_loop",array(
-                  		  											"MODULE_NAME" => $module['name'],
-                		  											"MODULE_LINK" => ROOT . "modules/" . $module['directory'] . "/" . $module['landingpage'],
-                       	 										));
+				$template->assign_block_vars("module_loop",array(
+																	"MODULE_NAME" => $module['name'],
+																	"MODULE_LINK" => ROOT . "modules/" . $module['directory'] . "/" . $module['landingpage'],
+																));
 			}
 
 			$template->assign_var('DATE',date("l jS F Y"));
                         
 			$template->set_filenames(array(
-                                            'head' => 'header.html',
-                                         ));
+											'head' => 'header.html',
+											));
 			$template->display('head');
 		}
 		
 		public static function pageFooter()
 		{
 			global $railway_name;
-            $template = new Template();
-            $template->set_custom_template(FROOT .'theme','default');
-            $template->assign_var('RAILWAY_NAME',$railway_name);
-            $template->assign_var('CURRENT_YEAR',gmdate("Y"));
-            if(isset($_SESSION['session_id']))
-            {
-            	$template->assign_block_vars('if_user_logged_in','');
-            }
-            $template->set_filenames(array(
-                                            'foot' => 'footer.html',
-                                        	));
-            $template->display('foot');
+			$template = new Template();
+			$template->set_custom_template(FROOT .'theme','default');
+			$template->assign_var('RAILWAY_NAME',$railway_name);
+			$template->assign_var('CURRENT_YEAR',gmdate("Y"));
+			if(isset($_SESSION['session_id']))
+			{
+				$template->assign_block_vars('if_user_logged_in','');
+			}
+			$template->set_filenames(array(
+											'foot' => 'footer.html',
+											));
+			$template->display('foot');
 		}
 		
 		// Modules
