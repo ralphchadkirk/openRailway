cb704356696ecf70fff4fe61392ee18221abbaa8
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-09 20:46:12 +0000

    Outline for permissions class

--- lib/perm.class.php
+++ lib/perm.class.php
@@ -1,14 +1,21 @@
 <?php
 	class Permissions extends Authentication
 	{
-        // Core OR permissions
-		const LOGIN = 1;
-		const MESSAGING = 2;
-		const ADMIN = 4;
-        const MODULES = 8;
-		
-		$simple_user = Permissions::LOGIN | Permissions::MESSAGING;
-		$user = $simple_user | Permissions::MODULES;
-		$admin = $user | Permissions::ADMIN;
+        private static function getUserPermission()
+		{
+			
+		}
+		private static function setUserPermission()
+		{
+			
+		}
+		private static function revokeUserPermission()
+		{
+			
+		}
+		private static function suspendUser()
+		{
+			
+		}
     }
 ?>
