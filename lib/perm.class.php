11bca727ae807a4314d078bd33c26612ace7b88d
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-13 10:32:39 +0000

    Base functions for class perm

--- lib/perm.class.php
+++ lib/perm.class.php
@@ -1,19 +1,30 @@
 <?php
 	class Permissions extends Authentication
 	{
-		public static function getUserPermission()
+		public static function getUserPermissions($uid)
 		{
 			
 		}
-		public static function setUserPermission()
+		public static function setUserPermission($uid,$pid)
 		{
 			
 		}
-		public static function revokeUserPermission()
+		public static function revokeUserPermission($uid,$pid)
+		{
+			// Query for user perm number here
+			// Query to get integer for pid
+			$user_perm ^= $perm_bit;
+			// Update user perm query here
+		}
+		public static function isUserAllowed($uid,$pid)
 		{
 			
 		}
-		public static function suspendUser()
+		public static function addNewPermission($name)
+		{
+			
+		}
+		public static function suspendUser($uid)
 		{
 			
 		}
