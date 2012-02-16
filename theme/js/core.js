29c1aef3d9899a5919008eddf42343fe1b3b7e02
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-16 10:32:46 +0000

    Removal of custom JS to core.js

---
+++ theme/js/core.js
@@ -0,0 +1,8 @@
+$('.alert-message').alert();
+$('.dropdown-toggle').dropdown();
+$('#activity-pane').modal();
+$(function () {   
+				$("[rel=tooltip]").tooltip({	
+												placement: "right"	
+										   }) 
+})
