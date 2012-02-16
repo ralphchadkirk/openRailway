a497d7b37ab4434c99591a9d4caf6b81d1300a08
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-02-16 21:21:15 +0000

    Messages folders and functions

--- user.php
+++ user.php
@@ -25,7 +25,63 @@
 				openRailwayCore::pageFooter();
 			break;
 			case "messages":
-				openRailwayCore::pageHeader("Your inbox");
+				if(isset($_GET['folder']))
+				{
+					$folder = $_GET['folder'];
+				}
+				else
+				{
+					$folder = "inbox";
+				}
+				$active_class = "class='active'";
+				$icon_white = "icon-white";
+				switch($folder):
+					case "inbox":
+						$title = "Inbox";
+						$name = "inbox";
+						$sql = "";
+					break;
+					case "outbox":
+						$title = "Outbox";
+						$name = "outbox";
+						$sql = "";
+					break;
+					case "sent":
+						$title = "Sent messages";
+						$name = "sent";
+						$sql = "";
+					break;
+					default:
+						trigger_error("An invalid folder has been provided",E_USER_WARNING);
+					break;
+				endswitch;
+				openRailwayCore::pageHeader($title,"Messages");
+				$template = new Template();
+				$template->set_custom_template("theme/" . STYLE,'default');
+				$template->assign_var('ROOT',ROOT);
+				$template->assign_var('TITLE',$title);
+				if(isset($name))
+				{
+					if($name == "inbox")
+					{
+					   $template->assign_var('INBOX_ACTIVE',$active_class);
+					   $template->assign_var('INBOX_WHITE',$icon_white);
+					}
+					elseif($name == "outbox")
+					{
+						$template->assign_var('OUTBOX_ACTIVE',$active_class);
+						$template->assign_var('OUTBOX_WHITE',$icon_white);
+					}
+					elseif($name == "sent")
+					{
+						$template->assign_var('SENT_ACTIVE',$active_class);
+						$template->assign_var('SENT_WHITE',$icon_white);
+					}
+				}
+				$template->set_filenames(array(
+												'body' => 'messages.html'
+											   ));
+				$template->display('body');
 				openRailwayCore::pageFooter();
 			break;
 			default:
