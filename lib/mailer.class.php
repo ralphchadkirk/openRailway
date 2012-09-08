24adb29b677d5a9c67dc2fc9834cd335895d9921
Ralph Chadkirk <ralphchadkirk@gmail.com>
2012-09-08 14:00:38 +0100

    Removal of mailer method

--- lib/mailer.class.php
+++ lib/mailer.class.php
@@ -3,10 +3,7 @@
 	{
 		public static function mailUser($uid,$subject,$content = null,$file)
 		{
-			if(isset($content) && isset($file))
-			{
-				trigger_error("File and content fields are mutually exclusive in mailUser()",E_USER_WARNING);
-			}
+
 		}
 	}
 ?>
