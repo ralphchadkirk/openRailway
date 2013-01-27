1de66f402242e12b4e7a3640e54554d27075d7ee
Ralph Chadkirk <ralphchadkirk@gmail.com>
2013-01-27 15:53:18 +0000

    Admin Panel updates

--- admincp/index.php
+++ admincp/index.php
@@ -8,17 +8,39 @@
 	{
 		$module = $_GET['module'];
 	}
+	else
+	{
+		$module = "stats";
+	}
 	if(isset($module))
 	{
 		switch ($module)
 		{
-			case "logs":
-					openRailwayCore::pageHeader("Activity Logs | Control Panel");
-					include(FROOT . "admincp/includes/layout.html");
-					openRailwayCore::pageFooter();
+			case "bug":
+					$title = "Report a Bug";
+					$active_var = "BUG";
+				break;
+			case "help":
+					$title = "Help & Support";
+					$active_var = "HELP";
 				break;
 			default:
+					$title = "Statistics";
+					$active_var = "STATS";
 				break;
 		}
+		
+		// Load layout
+		openRailwayCore::pageHeader($title . " | Control Panel");
+		$template = new Template;
+		$template->set_custom_template("includes/",'default');
+		$template->assign_var('ROOT',ROOT);
+		$template->assign_var($active_var,"active");
+		$template->assign_var('MAIN_TITLE',$title);
+		$template->set_filenames(array(
+									   'body' => 'layout.html'
+									   ));
+		$template->display('body');
+		openRailwayCore::pageFooter();
 	}
 ?>
