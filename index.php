61f40addf691e38c0749a6cac8f3713d08ff8e48
Ralph Chadkirk <ralphchadkirk@gmail.com>
2013-12-24 21:36:43 +0000

    New index page

--- index.php
+++ index.php
@@ -7,5 +7,12 @@
     Authentication::blockPageToVisitors();
 	
 	openRailwayCore::pageHeader("Your dashboard");
+	$template = new Template;
+	$template->set_custom_template("theme/" . STYLE,'default');
+	$template->set_filenames(array(
+								   'body' => 'home.html'
+								   ));
+	$template->display('body');
+
     openRailwayCore::pageFooter();
 ?>
