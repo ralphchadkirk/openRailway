d65d8ed5fac98affec634a4502912705099cdbbc
Ralph Chadkirk <ralphchadkirk@gmail.com>
2013-12-24 21:37:39 +0000

    Error logging table

--- admincp/log_err.php
+++ admincp/log_err.php
@@ -1,27 +1,35 @@
 <?php
 	Authentication::accessLevelController(8,'>');
-	$sql = "SELECT * FROM `log` WHERE `security_relevant` = '1' ORDER BY `event_id` DESC";
-	$result = openRailwayCore::dbQuery($sql);
 	
 	$main = new Template;
 	$main->set_custom_template("includes/",'default');
 	$main->assign_var('ROOT',ROOT);
 	
-	while($event = mysql_fetch_assoc($result))
+	if(isset($_GET['id']) && is_numeric($_GET['id']))
 	{
-		if(isset($event))
+		$main->assign_block_vars('if_detail_display','');
+	} else
+	{
+		$sql = "SELECT * FROM `log` WHERE `security_relevant` = '1' ORDER BY `event_id` DESC";
+		$result = openRailwayCore::dbQuery($sql);
+		$main->assign_block_vars('if_table_display','');
+		while($event = mysql_fetch_assoc($result))
 		{
-			$main->assign_block_vars('log_err',array(
-													 'ID' => $event['event_id'],
-													 'TIME' => date("d-M-Y H:i:s",$event['event_timestamp']),
-													 'SEV' => $event['event_severity'],
-													 'INTID' => $event['interaction_identifier'],
-													 'IP' => $event['source_ip'],
-													 'SUA' => $event['source_user_agent'],
-													 'DESC' => $event['description'],
-													 ));
+			if(isset($event))
+			{
+				$main->assign_block_vars('log_err',array(
+														 'ID' => $event['event_id'],
+														 'TIME' => date("d-M-Y H:i:s",$event['event_timestamp']),
+														 'SEV' => $event['event_severity'],
+														 'INTID' => $event['interaction_identifier'],
+														 'IP' => $event['source_ip'],
+														 'SUA' => $event['source_user_agent'],
+														 'DESC' => $event['description'],
+														 ));
+			}
 		}
 	}
+	
 	$main->set_filenames(array(
 							   'main' => "log_err.html"
 							   ));
