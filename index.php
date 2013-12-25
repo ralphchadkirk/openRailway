2c818ca5a6c69b560557d3e75d63272d039f4289
Ralph Chadkirk <ralphchadkirk@gmail.com>
2013-12-25 21:26:56 +0000

    IP Geolocation and user agent parsing

--- index.php
+++ index.php
@@ -6,9 +6,22 @@
 
     Authentication::blockPageToVisitors();
 	
+	// Process login info section
+	$ipAddr = $_SESSION['user_ip'];
+	$loginTime = date('d-m-Y h:i:s', $_SESSION['log_in_time']);
+	use phpbrowscap\Browscap;
+	$bc = new Browscap(FROOT . "cache");
+	// $browser = $bc->getBrowser(); COMMENTED OUT AS XAMPP DOES NOT SUPPORT
+	
 	openRailwayCore::pageHeader("Your dashboard");
 	$template = new Template;
 	$template->set_custom_template("theme/" . STYLE,'default');
+	$template->assign_var('IP_ADDR',$ipAddr);
+	$template->assign_var('LOGTIME',$loginTime);
+	if(isset($browser['parent']) && isset($browser['platform']))
+	{
+		$template->assign_var('BRWSR', $browser['parent'] . " on " . $browser['platform']);
+	}
 	$template->set_filenames(array(
 								   'body' => 'home.html'
 								   ));
